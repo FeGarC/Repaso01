081445c5f39c48c20e3cb5b34a6df9109e324223
fegar <fegarceo9606@gmail.com>
2023-04-10 20:29:52 -0500

    Repaso 01

---
+++ Repaso1.java
@@ -0,0 +1,23 @@
+//Crear un proyecto de Java y definir al menos 6 variables en tu IDE de distintos tipos de datos.
+package repaso1;
+
+public class Repaso1 {
+
+    
+    public static void main(String[] args) {
+        int numero = 7;
+        double decimal = -67.8;
+        float pi = (float) 3.14;
+        String palabra = "Hola Mundo";
+        boolean bandera = false;
+        String saludo = "Buenos días";
+        
+        System.out.println(numero);
+        System.out.println(decimal);
+        System.out.println(pi);
+        System.out.println(palabra);
+        System.out.println(bandera);
+        System.out.println(saludo);
+    }
+    
+}
